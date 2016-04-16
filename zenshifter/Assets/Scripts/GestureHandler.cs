b5206efb72d97a69ae5dcc228bb5dbc88331fe3e
elliotfiske <elliotfiske@gmail.com>
2016-04-16 13:14:25 -0700

    Dragging rows and cols works

---
+++ zenshifter/Assets/Scripts/GestureHandler.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+using TouchScript.Gestures;
+using System;
+
+public class GestureHandler : MonoBehaviour {
+
+	public GridScript grid;
+
+	// Register for pan gesture events
+	private void OnEnable() {
+		GetComponent<PanGesture>().PanStarted += PanBegan;
+		GetComponent<PanGesture>().Panned += PanMoved;
+		GetComponent<PanGesture>().PanCompleted += PanEnded;
+
+		GetComponent<TapGesture>().Tapped += Tapped;
+	}
+
+	private void OnDisable() {
+		GetComponent<PanGesture> ().PanStarted -= PanBegan;
+		GetComponent<PanGesture> ().Panned -= PanMoved;
+		GetComponent<PanGesture> ().PanCompleted -= PanEnded;
+
+		GetComponent<TapGesture> ().Tapped -= Tapped;
+	}
+
+	// Finger down!  Tell the grid, it'll start dragging a row or col if it can.
+	void PanBegan(object sender, EventArgs e) {
+		var panGesture = sender as PanGesture;
+		var worldPoint = Camera.main.ScreenToWorldPoint(panGesture.ScreenPosition);
+
+		// Determine if pan horizontal or vertical
+		var dir = panGesture.LocalDeltaPosition;
+		if (Math.Abs (dir.x) < Math.Abs (dir.y)) {
+			grid.TouchDownCol (worldPoint);	
+		}
+		else {
+			grid.TouchDownRow (worldPoint);
+		}
+	}
+
+	// Pan moved.  Tell the grid what the new offset is.
+	public void PanMoved(object sender, EventArgs e) {
+		var panGesture = sender as PanGesture;
+
+		grid.TouchMoved (panGesture.LocalDeltaPosition);
+	}
+
+	// Pan ended.  Tell the grid to snap to whatever.
+	void PanEnded(object sender, EventArgs e) {
+		grid.TouchEnded();
+	}
+
+	public void Tapped(object sender, EventArgs e) {
+		var tapGesture = sender as TapGesture;
+		var worldPoint = Camera.main.ScreenToWorldPoint(tapGesture.ScreenPosition);
+	}
+
+	// Use this for initialization
+	void Start () {
+		grid = GameObject.FindObjectOfType<GridScript> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ zenshifter/Assets/Scripts/GridScript.cs
@@ -0,0 +1,113 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class GridScript : MonoBehaviour {
+
+	public enum GridState { NoTouch, DraggingRow, DraggingCol};
+
+	public GridState state;
+
+	public GameObject square_template;
+	public GameObject circle_template;
+
+	public int dragging_row = -1;
+	public int dragging_col = -1;
+
+	private int num_rows = 5;
+	private int num_cols = 6;
+
+	// Multiply the x, y coords by the grid_size to get their pixel coords
+	private float grid_size = 1.85f;
+
+	public GameObject[][] grid;
+
+	// Set a row's "offset" to the specified amount.
+	void SlideRow(int row_ndx, float offset) {
+		for (int x = 0; x < num_cols; x++) {
+			var tile_pos = grid [row_ndx] [x].transform.localPosition;
+			tile_pos += new Vector3 (offset, 0);
+			grid [row_ndx] [x].transform.localPosition = tile_pos;
+		}
+	}
+
+	// Set a col's "offset" to the specified amount.
+	void SlideCol(int col_ndx, float offset) {
+		for (int y = 0; y < num_rows; y++) {
+			var tile_pos = grid [y] [col_ndx].transform.localPosition;
+			tile_pos += new Vector3 (0, offset);
+			grid [y] [col_ndx].transform.localPosition = tile_pos;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		// Pre populate the grid
+		grid = new GameObject[num_rows][];
+
+		for (int y = 0; y < num_rows; y++) {
+			grid [y] = new GameObject[num_cols];
+			for (int x = 0; x < num_cols; x++) {
+				grid [y] [x] = (GameObject) Instantiate (square_template, 
+					new Vector3(x * grid_size, y * grid_size, 0), Quaternion.identity);
+				grid [y] [x].transform.parent = transform;
+				grid [y] [x].transform.Translate (transform.position.x, transform.position.y, 0);
+			}
+		}
+	}
+
+	// Calculate which row we're dragging, if any
+	public void TouchDownRow(Vector3 world_point) {
+		var local_point = world_point - transform.position;
+		var raw_row = (local_point.y + grid_size / 2) / grid_size;
+
+		int row = Mathf.FloorToInt (raw_row);
+
+		if (row <= -1 || row >= num_rows) {
+			// Outta bounds.
+		} else {
+			dragging_row = row;
+			state = GridState.DraggingRow;
+		}
+	}
+
+	// Calculate which row we're dragging, if any
+	public void TouchDownCol(Vector3 world_point) {
+		var local_point = world_point - transform.position;
+		var raw_col = (local_point.x + grid_size / 2) / grid_size;
+
+		int col = Mathf.FloorToInt (raw_col);
+
+		if (col <= -1 || col >= num_cols) {
+			// Outta bounds.
+		} else {
+			dragging_col = col;
+			state = GridState.DraggingCol;
+		}
+	}
+
+	public void TouchMoved (Vector3 drag_offset) {
+		switch (state) {
+		case GridState.DraggingRow:
+			SlideRow (dragging_row, drag_offset.x);
+			break;
+		case GridState.DraggingCol:
+			SlideCol (dragging_col, drag_offset.y);
+			break;
+		default:
+			// Probably started drag not on a row.
+			break;
+		}
+	}
+
+	// Set state back to "not draggin fam"
+	public void TouchEnded() {
+		state = GridState.NoTouch;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
